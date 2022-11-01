8cbe77713e1f7b91689d4554812dd342fe26f5a2
José de Jesús Acevedoe <jose.de.jesus.acevedo.flores@gmail.com>
2022-11-01 07:37:34 -0600

    m

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import Header from './components/Header'
 import Section1 from './components/Section1'
 import Section2 from './components/Section2'  
 import Section3 from './components/Section3'
+import Section4 from './components/Section4'
 
 function App() {
 
@@ -16,6 +17,7 @@ function App() {
       <Section1/>
       <Section2/>
       <Section3/>
+      <Section4/>
     </div>
   )
 }
---
+++ src/components/Section4.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+const Section4 = () => {
+    return (
+        <div className='Section4'>
+            <h1>
+                ÚNETE A LA COMUNIDAD
+            </h1>
+            <p>
+                Nuestra comunidad está integrada por personas <br /> interesadas en la tecnología Web 3, emprendedores, <br /> empresas, desarrolladores y profesionales de la industria.
+            </p>
+            <button>
+                Llévame a Telegram
+            </button>
+
+        </div>
+    );
+};
+
+export default Section4;
