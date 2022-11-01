37a3ad23acbebc8793662bde4977eaf0164a97c9
José de Jesús Acevedoe <jose.de.jesus.acevedo.flores@gmail.com>
2022-11-01 08:12:49 -0600

    M

---
+++ src/components/Section5.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const Section5 = () => {
+    return (
+        <div className='section5'>
+            <div className='section5-a'>
+                <h1>
+                    Únete a nuestro newsletter
+                </h1>
+                <p>
+                    No te pierdas de las noticias más relevantes de web 3 cada mes.
+                </p>
+            </div>
+            <input type="text" />
+        </div>
+    );
+};
+
+export default Section5;     
